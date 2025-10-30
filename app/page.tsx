1938662518be598baf13d6f5724b57fe6c9aa135
ADSJohn <progjohn@outlook.com>
2025-10-30 16:20:44 -0300

    adicao do carousel de produtos

--- app/page.tsx
+++ app/page.tsx
@@ -10,14 +10,46 @@ export default function Home() {
     window.open(url, "_blank");
   };
 
+  const produtos = [
+    { descricao: 'Acido Lincap', image: '/images/acido-lincap.jpg' },
+    { descricao: 'Adigel', image: '/images/adigel.jpg' },
+    { descricao: 'Base amaciante', image: '/images/base-amaciante.jpg' },
+    { descricao: 'Devengel', image: '/images/denvergel-20kg.jpg' },
+    { descricao: 'Lessence', image: '/images/floarome.jpg' },
+    { descricao: 'Garrafa Branca 5L', image: '/images/garrafa-branca-5L.jpg' },
+    { descricao: 'Garrafa Cristal 5L', image: '/images/garrafa-cristal-5L.jpg' },
+    { descricao: 'Lauril pasta', image: '/images/lauril-pasta.jpg' },
+    { descricao: 'Sulfonico', image: '/images/sulfonico.jpg' },
+  ]
+
   return (
     <div>
-      <Card descricao="Sulfonico indiano" image="/images/sulfonico.jpg">
-        <Button
-          nome="Negociar"
-          onClick={() => handlerNegociar("Sulfonico indiano")}
-        />
-      </Card>
+      <div className="carousel-container">
+        <button className="carousel-btn left" onClick={() => {
+          const scroller = document.querySelector(".carousel")!;
+          scroller.scrollBy({ left: -300, behavior: "smooth" });
+        }}>
+
+        </button>
+
+        <div className="carousel">
+          {produtos.map((produto, item) => (
+            <Card key={item}
+              descricao={produto.descricao}
+              image={produto.image}
+            >
+              <Button nome="Negociar" onClick={() => handlerNegociar(produto.descricao)}>
+              </Button>
+            </Card>
+          ))}
+        </div>
+
+        <button className="carousel-btn left" onClick={() => {
+          const scroller = document.querySelector(".carousel")!;
+          scroller.scrollBy({ left: 300, behavior: "smooth" });
+        }}>
+        </button>
+      </div>
     </div>
   );
 }
