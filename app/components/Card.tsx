e90c617dea050e0fc2193ca9be1b36845dbf6c52
ADSJohn <progjohn@outlook.com>
2025-10-31 20:51:43 -0300

    ajustes

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,19 +2,19 @@ import { ReactNode } from "react";
 import Button from "./Button";
 
 interface CardProps {
-    descricao: string;
-    image: string;
-    children: ReactNode
+  descricao?: string;
+  image?: string;
+  children: ReactNode;
 }
 
 export default function Card({ descricao, image, children }: CardProps) {
-    return (
-        <div>
-            <div>
-                <h2>{descricao}</h2>
-            </div>
-            <img src={image} alt="" />
-            {children}
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <h2>{descricao}</h2>
+      </div>
+      {image && <img src={image} alt={descricao || ""} />}
+      {children}
+    </div>
+  );
 }
